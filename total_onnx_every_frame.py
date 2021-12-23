fixed incorrect dimensions bug in onnx script

total_onnx_every_frame.py
@@ -223,7 +223,7 @@ def getTrajectory(frame, input_width, input_height, bboxes, class_names):
 
     for label in detections_map:
         if label in detections_map and len(detections_map[label][0]) >= 8:
-            arr = np.array(detections_map[label], dtype=np.float32)
+            arr = np.array([detections_map[label][0][0:8]], dtype=np.float32)
             delta = arr[:, 1:, 0:2] - arr[:, :-1, 0:2]
 
             input_torch = torch.from_numpy(delta).to(device)